117da8db515a9386a0aa43b2d0b6eeca63b39b8c
tyommmm <artyomhovsepyan135@gmail.com>
2022-10-10 13:51:04 +0400

    Fully responsive NFT Website is ready!!! :)

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,11 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+// collapsible
+let coll = document.getElementsByClassName("collapsible");
+let i;
 
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
+    let content = this.nextElementSibling;
     if (content.style.maxHeight){
       content.style.maxHeight = null;
     } else {
@@ -12,3 +13,5 @@ for (i = 0; i < coll.length; i++) {
     } 
   });
 }
+
+
